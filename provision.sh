65f829a2ce66d2117bee102d076c85e2ef517682
Mikael Hogberg <mikael.hogberg@stendahls.se>
2015-02-17 08:16:47 +0100

    New provision code

--- provision.sh
+++ provision.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git pull
+wget https://github.com/mikaelnet/DeployMachine/archive/master.zip && unzip -o master.zip && rm master.zip
 xbuild DeployMachine.sln
 sudo mono DeployMachine/bin/Debug/DeployMachine.exe
-
