bdcaa2716b4dad7fbbdfc5631d0e9e0978c7a774
Mikael Hogberg <mikael.hogberg@stendahls.se>
2015-02-26 22:09:48 +0100

    Started on TC client

--- DeployMachine/Program.cs
+++ DeployMachine/Program.cs
@@ -4,13 +4,45 @@ using System.Linq;
 using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
 
 namespace DeployMachine
 {
+    class Options
+    {
+        [Option('h', "hostname", HelpText = "Hostname for TeamCity")]
+        public string TeamCityHost { get; set; }
+
+        [Option('u', "username", HelpText = "Username for TeamCity authentication")]
+        public string Username { get; set; }
+
+        [Option('p', "password", HelpText = "Password for TeamCity authentication")]
+        public string Password { get; set; }
+
+        [Option(HelpText = "Job ID to start")]
+        public string JobIdentity { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return "error message";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var options = new Options();
+            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                var helpText = HelpText.AutoBuild(options);
+                Console.WriteLine(helpText);
+                Environment.Exit(1);
+                return;
+            }
+
             try
             {
                 var deployManager = new DeployManager();
---
+++ DeployMachine/TeamCityClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace DeployMachine
+{
+    public class TeamCityClient
+    {
+        public string Hostname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string TaskId { get; set; }
+        public string State { get; set; }
+
+        public void StartJob(string jobid)
+        {
+            var url = string.Format("https://{0}/app/rest/buildQueue");
+            var buildXml = string.Format("<build><buildType id=\"{0}\"/></build>", jobid);
+
+            var request = (HttpWebRequest) WebRequest.Create(url);
+            var credentialsCache = new CredentialCache();
+            credentialsCache.Add(new Uri(url), "Basic", new NetworkCredential(Username, Password));
+            request.Credentials = credentialsCache;
+            request.Method = "POST";
+            request.ContentType = "application/xml";
+            request.Accept = "application/xml";
+
+            byte[] body = Encoding.UTF8.GetBytes(buildXml);
+            request.ContentLength = body.Length;
+            using (var putStream = request.GetRequestStream())
+            {
+                putStream.Write(body, 0, body.Length);
+            }
+            var response = (HttpWebResponse) request.GetResponse();
+            var xdoc = new XmlDocument();
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                xdoc.Load(reader);
+            }
+
+            TaskId = xdoc.DocumentElement.GetAttribute("taskId");
+            State = xdoc.DocumentElement.GetAttribute("state");
+        }
+
+        public void PollStatus()
+        {
+            
+        }
+    }
+}
